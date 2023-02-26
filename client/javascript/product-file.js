5a32c6901d0645e210d0a8a66a84a24e4823678b
Priyanshu <priyanshurouth@gmail.com>
2023-02-26 09:40:39 +0530

    Product file Updated

--- client/javascript/product-file.js
+++ client/javascript/product-file.js
@@ -4,11 +4,13 @@ form.addEventListener('submit',function(event){
     const formData = new FormData(form)
     const name = formData.get('name')
     const description=formData.get('description')
+    const sellername=formData.get('sellername')
     const price = formData.get('price')
     const image = formData.get('image')
     const user = {
         name,
         description,
+        sellername,
         price,
         image
     }
--- server/models/productmodel.js
+++ server/models/productmodel.js
@@ -22,7 +22,7 @@ const productSchema = new mongoose.Schema({
     required: true
   },
   image: {
-    type: Buffer,
+    type: String,
     required: true
   }
 });
